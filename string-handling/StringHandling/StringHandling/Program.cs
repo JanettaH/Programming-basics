28d4127b16da25e4afb92c3f0005cde157bf3553
JanettaH <janetta.huobonen@gmail.com>
2017-09-25 14:16:53 +0300

    StringHandling

---
+++ string-handling/StringHandling/StringHandling/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
