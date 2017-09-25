af6aef679c252698b31c03ed6e705828aa27f94a
JanettaH <janetta.huobonen@gmail.com>
2017-09-25 15:54:23 +0300

    Task 1 IF

---
+++ condition-statement/ConditionStatement/ConditionStatement/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConditionStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa positiivinen, negatiivinen tai nolla.");
+            Console.Write("Syötä Luku: ");
+            string userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+        }
+    }
+}
