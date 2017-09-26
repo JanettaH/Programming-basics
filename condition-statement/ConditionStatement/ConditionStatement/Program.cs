a8db7056226e37a1afa1a366e4f7eefd3c5f30cf
JanettaH <janetta.huobonen@gmail.com>
2017-09-26 09:40:58 +0300

    Condition Task One Done

--- condition-statement/ConditionStatement/ConditionStatement/Program.cs
+++ condition-statement/ConditionStatement/ConditionStatement/Program.cs
@@ -6,10 +6,34 @@ namespace ConditionStatement
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
             Console.WriteLine("Ohjelma tulostaa positiivinen, negatiivinen tai nolla.");
             Console.Write("Syötä Luku: ");
             string userInput = Console.ReadLine();
-            int number = int.Parse(userInput);
+            // int number = int.Parse(userInput);
+            bool isNumber = int.TryParse(userInput, out int number);
+
+            if (isNumber == true)
+            {
+                if (number > 0)
+                {
+                    Console.WriteLine($"Numero {number} on positiivinen");
+                }
+                else if (number < 0)
+                {
+                    Console.WriteLine($"Numero {number} on negatiivinen");
+                }
+                else
+                {
+                    Console.WriteLine("Numero 0 on nolla.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Vääräsyöte");
+            }
+            Console.ReadKey();
+
         }
     }
 }
