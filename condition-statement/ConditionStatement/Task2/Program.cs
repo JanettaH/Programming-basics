1d448c9efebb9bc2597f7a67c23572fc548a133d
JanettaH <janetta.huobonen@gmail.com>
2017-09-26 09:53:49 +0300

    Condition task 2 done

---
+++ condition-statement/ConditionStatement/Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma tulostaa parittoman tai parillisen");
+            Console.Write("Syötä Luku: ");
+            string userInput = Console.ReadLine();
+            // int number = int.Parse(userInput);
+            bool isNumber = int.TryParse(userInput, out int number);
+
+            if (isNumber == true)
+            {
+                if (number % 2 == 0)
+                    Console.WriteLine($"Numero {number} on parillinen");
+                else
+                    Console.WriteLine($"Numero {number} on pariton");
+            }
+            else
+                Console.WriteLine("Vääräsyöte");
+            Console.ReadKey();
+
+        }
+    }
+}
