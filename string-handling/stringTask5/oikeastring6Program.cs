16c1e16bcf4eb4186f3c988b08a0d411405abdf3
JanettaH <janetta.huobonen@gmail.com>
2017-10-03 09:52:29 +0300

    string handling tasks done

---
+++ string-handling/stringTask5/oikeastring6Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("syötä haluamasi teksti");
+            string userInput = Console.ReadLine().ToLower();
+
+            var vowels = new HashSet<char> { 'a', 'e', 'i', 'o', 'u' };
+
+            int total = 0;
+
+            for (int i = 0; i < userInput.Length; i++)
+            {
+
+                if (vowels.Contains(userInput[i]))
+                {
+                    total++;
+                }
+            }
+            Console.WriteLine("syöte sisältää {0} vokaalia", total);
+            Console.ReadLine();
+        }
+    }
+}
