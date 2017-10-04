a2c38aab54b6adfa004bfceb8eb090f354567020
JanettaH <janetta.huobonen@gmail.com>
2017-10-04 11:59:28 +0300

    kertaus tasks

---
+++ kertaus/kertaus/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace kertaus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("syötä luku");
+
+            string userInput = Console.ReadLine();
+            bool isNumber = int.TryParse(userInput, out int number);
+
+            if (number > 0)
+            {
+                Console.WriteLine("numero {0} on positiivinen", number);
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine("numero {0} on negatiivinen", number);
+            }
+            else (number == 0)
+            {
+                Console.WriteLine("luku {0} on nolla", number);
+            }
+            Console.WriteLine("press any key to continue");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ kertaus2/kertaus2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace kertaus2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("syötä luku");
+
+            string userInput = Console.ReadLine();
+            bool isNumber = int.TryParse(userInput, out int number);
+
+            if (isNumber == true)
+            {
+                if (number > 0)
+                {
+                    Console.WriteLine("numero {0} on positiivinen", number);
+                }
+                else if (number < 0)
+                {
+                    Console.WriteLine("numero {0} on negatiivinen", number);
+                }
+                else
+                {
+                    Console.WriteLine("luku {0} on nolla", number);
+                }
+            }
+            else
+            {
+                Console.WriteLine("syöte ei ole luku");
+            }
+            Console.WriteLine("press any key to continue");
+            Console.ReadKey();
+        }
+    }
+}
